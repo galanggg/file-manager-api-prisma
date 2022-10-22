2e36942c1ad1e6f8a9f158ddd5eda1d677f7eea0
galanggg <tugasgalang@gmail.com>
2022-10-22 12:56:30 +0800

    feat: initial prisma setup

---
+++ app.ts
@@ -0,0 +1,70 @@
+// eslint-disable-next-line
+require("dotenv").config()
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import { createApplication, createModule, gql } from "graphql-modules"
+import { Directory, FileVersion, File } from "@prisma/client"
+import { directoryModules } from "./directory"
+import { fileModules } from "./file"
+import { fileVersionModules } from "./fileVersion"
+const mainModule = createModule({
+  id: "main-module",
+  dirname: __dirname,
+  typeDefs: [
+    gql`
+      interface FileNode {
+        id: ID!
+        name: String!
+        createdAt: String!
+        updatedAt: String!
+      }
+
+      type Query {
+        searchFiles(query: String!): [FileNode]
+      }
+    `,
+  ],
+  resolvers: {
+    FileNode: {
+      __resolveType(obj: File | FileVersion | Directory) {
+        if (Object.prototype.hasOwnProperty.call(obj, "parentId")) {
+          return "Directory"
+        }
+        if (Object.prototype.hasOwnProperty.call(obj, "fileId")) {
+          return "FileVersion"
+        }
+        if (Object.prototype.hasOwnProperty.call(obj, "directoryId")) {
+          return "File"
+        }
+      },
+    },
+    Query: {
+      searchFiles: () => {
+        return "FileVersion"
+      },
+    },
+  },
+})
+
+const api = createApplication({
+  modules: [mainModule, directoryModules, fileModules, fileVersionModules],
+})
+
+const app = express()
+const port = 3000
+
+app.use(
+  "/graphql",
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  graphqlHTTP({
+    schema: api.schema,
+    customExecuteFn: api.createExecution(),
+    graphiql: process.env.NODE_ENV === "development",
+  })
+)
+
+app.get("/", (_, res) => res.send("Hello World"))
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}.`)
+})
